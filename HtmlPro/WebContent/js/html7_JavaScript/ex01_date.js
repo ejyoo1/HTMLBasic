734ad9d83e787902fca7911fe46ecaebd1b4d935
ejyoo1 <o3k3585@gmail.com>
2021-03-16 21:52:20 +0900

    html 소스관리 위한 백업

---
+++ HtmlPro/WebContent/js/html7_JavaScript/ex01_date.js
@@ -0,0 +1,7 @@
+/**
+ * Java Script 기본 - 외부 자바 스크립트
+ */
+
+var now = new Date(); //date 할당
+/*시스템 현재 시간 출력 : Fri Mar 12 2021 09:21:11 GMT+0900 (대한민국 표준시)*/
+document.write(now); //document내부 객체 사용하여 출력 (문서 출력)
